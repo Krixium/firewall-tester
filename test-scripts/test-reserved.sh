d204dafc91a3caf8a715534fd9d3eea53ff9b9c2
root <root@localhost.localdomain>
2019-01-24 13:32:40 -0800

    Changes

--- test-scripts/test-reserved.sh
+++ test-scripts/test-reserved.sh
@@ -4,14 +4,11 @@ to_host=$2
 
 cmd="hping3 -c 1"
 
-for port_num in {0..1023}
-do
-    $cmd -a $from_host -p $port_num -s 0 -S $to_host
-    $cmd -a $from_host -p $port_num -s 0 -SA $to_host
-    $cmd -a $from_host -p $port_num -s 0 -A $to_host
-    $cmd -a $from_host -p $port_num -s 0 -P $to_host
-    $cmd -a $from_host -p $port_num -s 0 -FA $to_host
-    $cmd -a $from_host -p $port_num -s 0 -RA $to_host
+$cmd -a $from_host -p 27557 -s 0 -S $to_host
+$cmd -a $from_host -p 27557 -s 0 -SA $to_host
+$cmd -a $from_host -p 27557 -s 0 -A $to_host
+$cmd -a $from_host -p 27557 -s 0 -P $to_host
+$cmd -a $from_host -p 27557 -s 0 -FA $to_host
+$cmd -a $from_host -p 27557 -s 0 -RA $to_host
 
-    $cmd -a $from_host -p $port_num -s 0 -2 $to_host
-done
+$cmd -a $from_host -p 27557 -s 0 -2 $to_host
